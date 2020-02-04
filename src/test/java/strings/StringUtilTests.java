1d42a4877da721b0818ea63afea214f39663abcc
lancelot520 <1014661165@qq.com>
2020-02-04 10:20:08 +0800

    init

---
+++ src/test/java/strings/StringUtilTests.java
@@ -0,0 +1,30 @@
+package strings;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class StringUtilTests {
+
+    @Test
+    public void testCosineSimilarity() throws IOException {
+        List<Byte> tokenX = new ArrayList<>();
+        for (int i=1; i<=7; i++){
+            tokenX.add((byte)i);
+        }
+        List<Byte> tokenY = new ArrayList<>();
+        for (int i=1; i<=3; i++){
+            tokenY.add((byte)i);
+        }
+        for (int i=8; i<=11; i++){
+            tokenY.add((byte)i);
+        }
+
+        double similarity = StringUtil.cosineSimilarity(tokenX, tokenX);
+        System.out.println(similarity);
+    }
+}
