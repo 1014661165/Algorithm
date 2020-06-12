da2b801c299f9d7de130ab96fd30b403808b1f29
lylf <1014661165@qq.com>
2020-06-12 13:58:19 +0800

    update

---
+++ src/test/java/strings/JieBaTests.java
@@ -0,0 +1,20 @@
+package strings;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+import org.junit.Test;
+import org.wltea.analyzer.core.IKSegmenter;
+import org.wltea.analyzer.core.Lexeme;
+import org.wltea.analyzer.lucene.IKAnalyzer;
+
+import java.io.StringReader;
+
+public class JieBaTests {
+
+
+
+    @Test
+    public void cutSentence()throws Exception{
+    }
+}
